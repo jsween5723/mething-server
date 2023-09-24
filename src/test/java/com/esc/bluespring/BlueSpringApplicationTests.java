52711f784e1b948970d35802cc96766e372956ca
jsween5723 <jsween5723@gmail.com>
2023-09-24 11:01:31 +0900

    test(main): 설정서버 넘기기

--- src/test/java/com/esc/bluespring/BlueSpringApplicationTests.java
+++ src/test/java/com/esc/bluespring/BlueSpringApplicationTests.java
@@ -1,13 +1,12 @@
 package com.esc.bluespring;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BlueSpringApplicationTests {
 
-  @Test
-  void contextLoads() {
-  }
+//  @Test
+//  void contextLoads() {
+//  }
 
 }
