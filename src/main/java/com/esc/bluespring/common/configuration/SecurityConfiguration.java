9c8140e65780bbf936ebb8be1bf148f4c170771d
jsween5723 <jsween5723@gmail.com>
2023-11-05 15:14:12 +0900

    feat: same-site none

--- src/main/java/com/esc/bluespring/common/configuration/SecurityConfiguration.java
+++ src/main/java/com/esc/bluespring/common/configuration/SecurityConfiguration.java
@@ -40,7 +40,8 @@ public class SecurityConfiguration {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable).cors(
                 httpSecurityCorsConfigurer -> httpSecurityCorsConfigurer.configurationSource(
-                    corsConfigurationSource)).authorizeHttpRequests(
+                    corsConfigurationSource))
+            .authorizeHttpRequests(
                 registry -> registry.requestMatchers("/swagger-ui/**").hasRole(Role.ADMIN.name())
                     .requestMatchers("/resources").permitAll().anyRequest().permitAll())
             .oauth2ResourceServer(oauth2 -> oauth2.jwt(
