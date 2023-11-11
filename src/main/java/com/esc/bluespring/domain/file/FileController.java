157ddac05504766b1752db9f59ddb687e630567a
jsween5723 <jsween5723@gmail.com>
2023-11-12 02:22:46 +0900

    feat(file): 비로그인 업로드

--- src/main/java/com/esc/bluespring/domain/file/FileController.java
+++ src/main/java/com/esc/bluespring/domain/file/FileController.java
@@ -1,9 +1,12 @@
 package com.esc.bluespring.domain.file;
 
+import static com.esc.bluespring.domain.member.entity.Member.ANONYMOUS;
+
 import com.esc.bluespring.common.BaseResponse;
 import com.esc.bluespring.common.utils.file.S3Service;
 import com.esc.bluespring.domain.file.entity.Image;
 import com.esc.bluespring.domain.file.model.FileDto.FileUrlResponse;
+import jakarta.annotation.security.RolesAllowed;
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
@@ -20,6 +23,7 @@ public class FileController {
     private final S3Service s3Service;
 
     @PostMapping(value = "image", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @RolesAllowed({ANONYMOUS})
     public BaseResponse<FileUrlResponse> uploadImage(@NotNull MultipartFile file) {
         Image image = s3Service.upload(file);
         return new BaseResponse<>(new FileUrlResponse(image.getUrl()));
