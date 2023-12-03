485b2447a5a513e69b5e59d141976dd8e5c153ac
jsween5723 <jsween5723@gmail.com>
2023-12-03 15:59:21 +0900

    fix(file): enable url encodeing

--- src/main/java/com/esc/bluespring/domain/file/FileService.java
+++ src/main/java/com/esc/bluespring/domain/file/FileService.java
@@ -3,15 +3,18 @@ package com.esc.bluespring.domain.file;
 
 import com.esc.bluespring.domain.file.entity.Image;
 import com.esc.bluespring.domain.file.entity.ImageRepository;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class FileService {
+
     private final ImageRepository imageRepository;
 
     public Image findByUrl(String url) {
-        return imageRepository.findByUrl(url);
+        return imageRepository.findByUrl(URLEncoder.encode(url, StandardCharsets.UTF_8));
     }
 }
